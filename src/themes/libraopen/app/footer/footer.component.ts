e966a7a7f2f84dfa5c0c27d4cdb28e173d3ceb83
Nestor Walker <naw4t@virginia.edu>
2026-02-23 11:20:10 -0500

    Use the themed footer

--- src/themes/libraopen/app/footer/footer.component.ts
+++ src/themes/libraopen/app/footer/footer.component.ts
@@ -10,10 +10,10 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
 
 @Component({
   selector: 'ds-themed-footer',
-  // styleUrls: ['./footer.component.scss'],
-  styleUrls: ['../../../../app/footer/footer.component.scss'],
-  // templateUrl: './footer.component.html'
-  templateUrl: '../../../../app/footer/footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  // styleUrls: ['../../../../app/footer/footer.component.scss'],
+  templateUrl: './footer.component.html',
+  // templateUrl: '../../../../app/footer/footer.component.html',
   standalone: true,
   imports: [
     AsyncPipe,

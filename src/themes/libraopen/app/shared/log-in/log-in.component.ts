82cb01ac1ad13ed51b2912ca6a652dcdbd2a3c7e
Nestor Walker <naw4t@virginia.edu>
2026-03-18 14:12:51 -0400

    Removed ORCID from login component

--- src/themes/libraopen/app/shared/log-in/log-in.component.ts
+++ src/themes/libraopen/app/shared/log-in/log-in.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { AuthMethodType } from '../../../../../app/core/auth/models/auth.method-type';
 import { ThemedLoadingComponent } from '../../../../../app/shared/loading/themed-loading.component';
 import { LogInContainerComponent } from '../../../../../app/shared/log-in/container/log-in-container.component';
 import { LogInComponent as BaseComponent } from '../../../../../app/shared/log-in/log-in.component';
@@ -19,4 +20,13 @@ import { LogInComponent as BaseComponent } from '../../../../../app/shared/log-i
   ],
 })
 export class LogInComponent extends BaseComponent {
+
+  override ngOnInit(): void {
+    // Libra Open: Exclude ORCID login from nav menu and login page
+    // Users log in with ORCID on person pages, not from the nav menu or login page.
+    if (this.excludedAuthMethod == null) {
+      this.excludedAuthMethod = AuthMethodType.Orcid;
+    }
+    super.ngOnInit();
+  }
 }
